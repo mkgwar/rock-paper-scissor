74d6b67de59f7bfba5242e8a8d3b577867a31835
mkgwar <mohitgangwarknp@gmail.com>
2022-01-23 20:24:51 +0530

    responsive issues fix

--- src/Components/Coin.js
+++ src/Components/Coin.js
@@ -1,6 +1,8 @@
 const Coin = ({ url, coinStyle }) => {
   return (
-    <div className={"h-40 w-40 p-4 border-b-8 rounded-full " + coinStyle}>
+    <div
+      className={"h-[20vh] w-[20vh] p-4 border-b-8 rounded-full " + coinStyle}
+    >
       <div className="bg-white w-full h-full flex justify-center items-center border-t-8 border-gray-200 rounded-full">
         <img src={url} alt="paper" />
       </div>
--- src/Components/MainGame.js
+++ src/Components/MainGame.js
@@ -6,7 +6,7 @@ const MainGame = () => {
   return (
     <div className="main-game h-full w-full flex justify-center items-center">
       <div className="flex flex-col items-center w-full gap-16">
-        <div className="flex justify-center items-center w-full gap-32">
+        <div className="flex justify-center items-center w-full gap-12">
           <Paper />
           <Scissors />
         </div>
