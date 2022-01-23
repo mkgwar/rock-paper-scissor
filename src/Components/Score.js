494e1a13977864462487565afdd913037420a1c0
mkgwar <mohitgangwarknp@gmail.com>
2022-01-23 20:33:53 +0530

    changed text size from rem to vh

--- src/Components/Score.js
+++ src/Components/Score.js
@@ -1,16 +1,16 @@
 const Score = () => {
   return (
     <div className="flex justify-between items-center border-2 border-Header-Outline p-4 absolute top-0 left-0 right-0 rounded-lg">
-      <div className="text-white leading-5 text-2xl font-black">
+      <div className="text-white leading-[2.5vh] text-[2.5vh] font-black">
         ROCK
         <br />
         PAPER
         <br />
         SCISSORS
       </div>
-      <div className="bg-white p-6 py-2 flex flex-col items-center">
-        <div className="text-Score-Text text-sm">SCORE</div>
-        <div className="text-Dark-Text text-5xl">12</div>
+      <div className="bg-white p-6 py-2 flex flex-col items-center justify-center">
+        <div className="text-Score-Text text-[1.5vh]">SCORE</div>
+        <div className="text-Dark-Text text-[4.5vh]">12</div>
       </div>
     </div>
   );
